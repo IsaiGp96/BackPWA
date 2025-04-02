feaaf837f8f535c919a796a7bbe2779901b0ba01
IsaiGp96 <abraham_gp96@outlook.com>
2025-04-01 20:07:15 -0600

    Creating .env file for production test

--- src/config/dbConfig.js
+++ src/config/dbConfig.js
@@ -1,14 +1,26 @@
-// Inicializa Firebase Admin SDK con la clave de servicio
+// dbConfig.js
 
-    const serviceAccount = require('./testc78b75firebaseadminsdk.json');
-    const admin = require('firebase-admin');
+require('dotenv').config();
+const admin = require('firebase-admin');
 
-    admin.initializeApp({
-      credential: admin.credential.cert(serviceAccount),
-    });
-    
-    const db = admin.firestore();
+const serviceAccount = {
+  type: "service_account",
+  project_id: "testc-78b75",
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
+  client_email: "firebase-adminsdk-rlzaw@testc-78b75.iam.gserviceaccount.com",
+  client_id: "108741476290555512881",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url: "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-rlzaw%40testc-78b75.iam.gserviceaccount.com",
+  universe_domain: "googleapis.com"
+};
 
-    module.exports = db
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
+const db = admin.firestore();
 
+module.exports = db;
